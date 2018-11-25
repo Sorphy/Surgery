d7db91e687c3feb19748109b6802b73ceff59a5c
Ondra Szkandera <szkanderao@gmail.com>
2018-11-25 14:47:05 +0100

    Initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Ondra\Desktop\Projects\Python\Surgery\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
